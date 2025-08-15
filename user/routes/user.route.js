6327c8c61e2fe279eb876df91f5d487a9e2e1515
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-08-15 15:00:46 +0530

    tested user service on postman

--- user/routes/user.route.js
+++ user/routes/user.route.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/auth")
 const router = express.Router()
 
 router.post('/register', UserController.register)
-router.post('/login', UserController.login)
+router.post('/login',  UserController.login)
 router.get('/logout', UserController.logout)
 router.get('/profile', authMiddleware.userAuth, UserController.profile)
 
